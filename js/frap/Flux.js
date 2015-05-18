0cd681b7bdfeaad8bf0a4ce0f974bb54547a5bb9
directionGraffiti <direction@urban-radio.com>
2015-05-18 18:04:49 +0200

    www

--- js/frap/Flux.js
+++ js/frap/Flux.js
@@ -1,6 +1,8 @@
 Flux = {
 
-    _url_flux : 'http://www.urban-radio.com/appli/graffiti.js',//http://80.82.229.204/datasun/bocetoplayer/frap.js',
+	_url_flux : 'graffiti.js',
+
+    //_url_flux : 'http://www.urban-radio.com/appli/graffiti.js',//http://80.82.229.204/datasun/bocetoplayer/frap.js',
 
     _frap_flux : '',
 
@@ -9,26 +11,28 @@ Flux = {
     },
 
     getFlux : function() {
-		$.ajax({ 
-			type: 'GET',
-			url: Flux._url_flux,
-			timeout: 3000,
-			success: function(data) {
-				Flux._frap_flux = data;
-				if(Flux._frap_flux) {
-					Podcasts.init();
-					//Videos.init();
-					Api.init();
-					Player.init();
-					Direct.init();
-				} else {
-					alert('connexion error flux');
+		$(document).ready(function() {
+			$.ajax({ 
+				type: 'GET',
+				url: Flux._url_flux,
+				timeout: 3000,
+				success: function(data) {
+					Flux._frap_flux = data;
+					if(Flux._frap_flux) {
+						Podcasts.init();
+						//Videos.init();
+						Api.init();
+						Player.init();
+						Direct.init();
+					} else {
+						alert('connexion error flux');
+					}
+				},
+				error: function() {
+					console.log('error de connexion au FLUX');
 				}
-			},
-			error: function() {
-				console.log('error de connexion au FLUX');
-			}
-		});
+			});
+		}); //fin script Jquery  
     },
 
 }
