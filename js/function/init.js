ae69ceabf3548d84e61d6be09a67e069541a4f62
directionGraffiti <direction@urban-radio.com>
2015-05-21 16:45:49 +0200

    connect

--- js/function/init.js
+++ js/function/init.js
@@ -67,19 +67,8 @@ $(document).ready(function () {
 	//jembe.control.listenKey('82', openMenuAndroid);
 	
 	function checkConnection() {
-		var networkState = navigator.connection.type;
 	
-		var states = {};
-		states[navigator.connection.UNKNOWN]  = 'Unknown connection';
-		states[navigator.connection.ETHERNET] = 'Ethernet connection';
-		states[navigator.connection.WIFI]     = 'WiFi connection';
-		states[navigator.connection.CELL_2G]  = 'Cell 2G connection';
-		states[navigator.connection.CELL_3G]  = 'Cell 3G connection';
-		states[navigator.connection.CELL_4G]  = 'Cell 4G connection';
-		states[navigator.connection.CELL]     = 'Cell generic connection';
-		states[navigator.connection.NONE]     = 'No network connection';
-	
-		alert('Connection type: ' + states[networkState]);
+		alert(navigator.connection.type);
 	}
 	checkConnection();
 
