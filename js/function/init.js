b115f877003c0806d3c9430bbcd588edde38156b
directionGraffiti <direction@urban-radio.com>
2015-05-21 12:32:00 +0200

    status

--- js/function/init.js
+++ js/function/init.js
@@ -25,6 +25,9 @@ if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i)
 if(navigator.userAgent.match(/Android/i)) isAndroid2 = true;
 jembe.internet.listen(onOffline);
 
+console.log(">---jembe.internet.status---<");
+console.log(jembe.internet.status);
+
 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
